8b01ac7eeda9e18f3533b7744daa1bc0e2f1da3a
Chris <111382648+CalpiCHO@users.noreply.github.com>
2023-09-15 15:09:00 -0700

    Completed round conditions between player and computer. Incorporated function into index.html

--- index.js
+++ index.js
@@ -1,15 +1,76 @@
 function getComputerChoice() {
-    num = Math.floor(Math.random() * 3) + 1
-    switch (num) {
-        case 1:
-            ans = "rock";
+    computerSelection = Math.floor(Math.random() * 3) + 1
+    // switch (num) {
+    //     case 1:
+    //         ans = "rock";
+    //         break;
+    //     case 2: 
+    //         ans = "paper";
+    //         break;
+    //     case 3:
+    //         ans = "scissors";
+    //         break;
+    // }
+    return computerSelection;
+}
+
+function playerSelection() {
+    rps = prompt("Please choose rock, paper, or scissors!").toLowerCase();
+    switch (rps) {
+        case "rock":
+            playerSelection = 1;
             break;
-        case 2: 
-            ans = "paper";
+        case "paper":
+            playerSelection = 2;
             break;
-        case 3:
-            ans = "scissors";
+        case "scissors": 
+        playerSelection = 3;
+            break;
+        default:
+            alert("Oops! You entered something incorrectly! Try again")
             break;
     }
-    return ans;
+    return playerSelection
+}
+
+// rock = 1
+// paper = 2
+// scissors = 3
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 1 && computerSelection === 1) {
+        alert("Rock ties rock! Try again!")
+    }
+
+    if (playerSelection === 1 && computerSelection === 2) {
+        alert("Paper beats rock! You lose!")
+    }
+
+    if (playerSelection === 1 && computerSelection === 3) {
+        alert("Rock beats scissors! You win!")
+    }
+
+    if (playerSelection === 2 && computerSelection === 1) {
+        alert("Paper beats rock! You win!")
+    }
+
+    if (playerSelection === 2 && computerSelection === 2) {
+        alert("Paper ties paper! Try again!")
+    }
+
+    if (playerSelection === 2 && computerSelection === 3) {
+        alert("Scissors beats paper! You lose!")
+    }
+
+    if (playerSelection === 3 && computerSelection === 1) {
+        alert("Rock beats scissors! You lose!")
+    }
+
+    if (playerSelection === 3 && computerSelection === 2) {
+        alert("Scissors beats paper! You win!")
+    }
+
+    if (playerSelection === 3 && computerSelection === 3) {
+        alert("Scissors ties scissors! Try again!")
+    }
 }
